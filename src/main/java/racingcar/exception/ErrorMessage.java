4e40dfe50ad676686cc2e61fe0dcc8899921e741
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-10-24 01:42:41 +0900

    style(view): 불필요한 주석 및 공백 제거

--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -1,16 +1,12 @@
 package racingcar.exception;
 
 public enum ErrorMessage {
-    // 공통 입력 검증
+    
     BLANK_INPUT("입력값이 비어있습니다."),
-
-    // 자동차 이름 관련
     EMPTY_CAR_NAME("자동차 이름이 비어있습니다."),
     CAR_NAME_LENGTH_EXCEEDED("자동차 이름은 5자 이하여야 합니다."),
     DUPLICATE_CAR_NAME("자동차 이름이 중복되었습니다."),
     CAR_COUNT_EXCEEDED("자동차는 최대 100대까지 입력 가능합니다."),
-
-    // 시도 횟수 관련
     INVALID_ROUND_FORMAT("시도 횟수는 숫자로 입력해야 합니다."),
     ROUND_NOT_POSITIVE("시도 횟수는 양수여야 합니다.");
 
