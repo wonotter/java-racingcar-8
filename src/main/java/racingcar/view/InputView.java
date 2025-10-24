b8e2eeb0813ee646f07180196015866079fc2a4e
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-10-24 20:55:09 +0900

    chore(view): InputView 공백 trim 기능 추가
    자동차 이름을 공백 포함 입력 시 공백은 제거할 수 있도록 하는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,25 +3,30 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import racingcar.exception.ErrorMessage;
 
 public class InputView {
-    
+
     private static final String CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String ROUNDS_PROMPT = "시도할 횟수는 몇 회인가요?";
     private static final String DELIMITER = ",";
 
     public List<String> readCarNames() {
         System.out.println(CAR_NAMES_PROMPT);
+
         String input = Console.readLine();
         validateNotBlank(input);
+
         return parseCarNames(input);
     }
 
     public int readRounds() {
         System.out.println(ROUNDS_PROMPT);
+
         String input = Console.readLine();
         validateNotBlank(input);
+
         return parseRounds(input);
     }
 
@@ -32,7 +37,9 @@ public class InputView {
     }
 
     private List<String> parseCarNames(String input) {
-        return Arrays.asList(input.split(DELIMITER));
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
     }
 
     private int parseRounds(String input) {
